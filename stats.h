c5f198c64b9efe24d68a1e63154ba6368397d0d1
RahulB <48803973+RahulB117@users.noreply.github.com>
2022-05-15 00:16:34 +0530

    Removed Typedef from stats.h

--- stats.h
+++ stats.h
@@ -1,14 +1,14 @@
-typedef struct
+struct Stats
 {
     float average;
     float min;
     float max;
-}Stats;
+};
 
-Stats compute_statistics(const float* numberset, int setlength);
+struct Stats compute_statistics(const float* numberset, int setlength);
 
 typedef int (*alerter_funcptr)(float, float);
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats computedStats);
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
